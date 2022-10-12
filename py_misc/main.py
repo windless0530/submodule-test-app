c9a184814d7a6adb99739bc167e625b66dd81994
Elio <elio@autra.tech>
2022-10-12 15:29:56 +0800

    python type hint test

---
+++ py_misc/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+file: main.py
+description: 放一些杂七杂八的东西
+author: 张翼
+date: 2022-10-12 14:40:00
+"""
+
+
+def _f(a: int):
+    return a * 2
+
+
+if __name__ == "__main__":
+    # print(_f("a"))
+    print(_f(10))
